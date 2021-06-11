e5f1e39b93b854062b3ddad3ca60e5a1db9f74cc
YaroslawGarbarenko <72256923+YaroslawGarbarenko@users.noreply.github.com>
2021-06-11 09:55:17 +0300

    Add files via upload

---
+++ экз/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace экз
+{
+    class Cinema
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <100; i++)
+            {
+                Visitors visitors = new Visitors(i);
+            }
+            Console.ReadKey();
+        }
+        class Visitors
+        {
+            private static Semaphore sem = new Semaphore(3, 3);
+            Thread visitorsThread;
+            int count = 5;
+
+
+            public Visitors(int i)
+            {
+                visitorsThread = new Thread(visit);
+                visitorsThread.Name = $"Visitors{i.ToString()}";
+                visitorsThread.Start();
+            }
+            public void visit()
+            {
+                while (count > 0)
+                {
+                    sem.WaitOne();
+
+                    Console.WriteLine($"{Thread.CurrentThread.Name} First Room");
+                    Console.WriteLine($"{Thread.CurrentThread.Name} Second Room");
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"{Thread.CurrentThread.Name} Third Room");
+                    sem.Release();
+                    count--;
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
